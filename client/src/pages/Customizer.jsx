696ff88be3b3f615a8ba5f1522e2e65dab5bdb0a
Mohamed Aseem <ima7str@gmail.com>
2026-03-08 02:21:16 +0530

    Restructure project with client and server folders

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -41,10 +41,28 @@ function Customizer() {
           readFile={readFile}
         />
       case "aipicker":
-        return <AIPicker />
+        return <AIPicker
+          prompt={prompt}
+          setPrompt={setPrompt}
+          generatingImg={generatingImg}
+          handleSubmit={handleSubmit}
+        />
 
       default:
         return null
+    }
+  }
+
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert("Please enter a prompt");
+
+    try {
+      // call our backemd to generaye am ai image!
+    } catch (error) {
+      alert(error)
+    } finally {
+      setGeneratingImg(false)
+      setActiveEditorTab("")
     }
   }
 
